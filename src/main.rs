e32fd87743aaf5b7921c7dbada6e101fe076ed94
oushesh <oushesh@gmail.com>
2024-01-03 18:00:25 +0400

    updated: Cargo and src for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use axum::{routing::get, Router};
+//use axum_openapi::swagger_ui_handler;
+//use axum_openapi::swagger_ui_handler;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -12,9 +14,12 @@ async fn goodbye() -> &'static str{
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world))
+    let router = Router::new()
+        .route("/", get(hello_world))
         .route("/greet",get(greet))
         .route("/goodbye",get(goodbye));
+        //s.nest("/docs",swagger_ui_handler("/openapi.json"));
+
     Ok(router.into())
 }
 
